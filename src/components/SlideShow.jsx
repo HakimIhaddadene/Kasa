61abb7251059ac9d9d7be83603e88e18520f691d
Hakim Ihaddadene <hakim.ihaddadene@gmail.com>
2023-02-13 00:22:28 +0100

    Ajout du composant SlideShow

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,12 +1,75 @@
-import React from "react"
+import React from 'react'
+import { useState } from 'react'
+import styled from 'styled-components'
+import rightArrow from '../images/arrows/right-arrow-slide-show.png'
+import leftArrow from '../images/arrows/left-arrow-slide-show.png'
 
-function SlideShow({pictures}) {
+const SlideShowContainer = styled.div`
+width: 100%;
+position: relative;
+`
 
-   //  const [picture, setPicture] = useState(0);
+const Image = styled.img`
+width: 100%;
+height: 415px;
+object-fit: cover;
+border-radius: 25px;
+`
+
+const RightArrowContainer = styled.div`
+display: flex;
+position: absolute;
+right: 10px;
+top: 167px;
+cursor: pointer;
+background-color: transparent;
+`
+
+const LeftArrowContainer = styled.div`
+display: flex;
+position: absolute;
+left: 10px;
+top: 167px;
+cursor: pointer;
+background-color: transparent;
+`
+
+function SlideShow({pics}) {
+
+    const [picture, setPicture] = useState(0);
+    const numberOfPictures = pics.length;
+
+    const nextPicture = () => {
+        setPicture(picture === numberOfPictures - 1 ? 0 : picture + 1);
+    };
+
+    const previousPicture = () => {
+        setPicture(picture === 0 ? numberOfPictures - 1 : picture - 1);
+    };  
 
     return (
         <div>
-
+            <SlideShowContainer>
+                <RightArrowContainer>
+                    {numberOfPictures > 1 && (
+                        <img src={rightArrow} alt="Image suivante" onClick={nextPicture} />
+                    )}     
+                </RightArrowContainer> 
+                <LeftArrowContainer>       
+                    {numberOfPictures > 1 && (
+                        <img src={leftArrow} alt="Image précédente" onClick={previousPicture} />
+                    )}
+                </LeftArrowContainer>
+                {pics.map((image, index) => {
+                    return (
+                    <div key={index}>
+                        {index === picture && (
+                        <Image img src={image} alt="#" />    
+                        )}
+                    </div>
+                    );
+                })}   
+            </SlideShowContainer>
         </div>
     )
 }
